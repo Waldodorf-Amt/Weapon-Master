f203eb05ec8e484cfc4df5fbefa49f87bccbccd7
Waldodorf-Amt <sibear2780@gmail.com>
2026-05-12 14:45:55 -0400

    Add player cache data for Ebonmarch

---
+++ ebonmarch-player-cache.js
@@ -0,0 +1,102 @@
+window.EBONMARCH_PLAYER_CACHE = [
+  {
+    orkId: "102086",
+    name: "Aathaeus Draew'ind",
+    park: "Grim Garrison"
+  },
+  {
+    orkId: "180118",
+    name: "Agni",
+    park: "Direwolf's Rest"
+  },
+  {
+    orkId: "106877",
+    name: "Aiko The Crimson Rose Kawashima",
+    park: "The Mire"
+  },
+  {
+    orkId: "184228",
+    name: "Alex",
+    park: "Grim Garrison"
+  },
+  {
+    orkId: "175761",
+    name: "Alfie",
+    park: "Direwolf's Rest"
+  },
+  {
+    orkId: "186452",
+    name: "Alrich",
+    park: "Direwolf's Rest"
+  },
+  {
+    orkId: "180120",
+    name: "Andy Brohs",
+    park: "Direwolf's Rest"
+  },
+  {
+    orkId: "174070",
+    name: "Aneurae Mooncrest",
+    park: "Storm Bridge"
+  },
+  {
+    orkId: "7659",
+    name: "Archduess Duchess Baron Sage Lorde Masta Kai Darkjester Clayd,Esquire,Warden of the West,Defender",
+    park: "Storm Bridge"
+  },
+  {
+    orkId: "162651",
+    name: "Armenius",
+    park: "Direwolf's Rest"
+  },
+  {
+    orkId: "187238",
+    name: "Asher",
+    park: "The Mire"
+  },
+  {
+    orkId: "61633",
+    name: "Athador",
+    park: "Kirin Grove"
+  },
+  {
+    orkId: "108140",
+    name: "Atreides",
+    park: "Grim Garrison"
+  },
+  {
+    orkId: "181447",
+    name: "Austin",
+    park: "The Mire"
+  },
+  {
+    orkId: "109279",
+    name: "Axel",
+    park: "Grim Garrison"
+  },
+  {
+    orkId: "184277",
+    name: "Azergoth Raulgore",
+    park: "The Mire"
+  },
+  {
+    orkId: "187237",
+    name: "Bamby",
+    park: "The Mire"
+  },
+  {
+    orkId: "171786",
+    name: "Barkson Tree Hugger",
+    park: "The Mire"
+  },
+  {
+    orkId: "149527",
+    name: "Baron Rowan Crowe",
+    park: "Ebonmarch"
+  },
+  {
+    orkId: "52177",
+    name: "Baroness Lady Robyn Shutterbug",
+    park: "Storm Bridge"
+  }
+];
